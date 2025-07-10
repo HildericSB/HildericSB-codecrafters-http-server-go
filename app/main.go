089520682568e20bcc6f254ed30fef6f0570cbf8
Hilde <hilderic.bourin@gmail.com>
2025-07-10 17:05:04 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,6 +11,8 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+const CRLF = "\r\n"
+
 func main() {
 
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -39,25 +41,37 @@ func main() {
 	lines := strings.Split(req, "\r\n")
 	path := strings.Split(lines[0], " ")[1]
 
-	fmt.Println(path)
+	// Put HTTP headers in a map
+	headers := make(map[string]string)
+	for _, line := range lines[1:] {
+		// If line is empty, there is no more header
+		if line == "" {
+			break
+		}
+		headerSplit := strings.SplitN(line, ":", 2)
+		for i, v := range headerSplit {
+			headerSplit[i] = strings.TrimSpace(v)
+		}
+		headers[headerSplit[0]] = headerSplit[1]
+	}
+
+	fmt.Println("Path : " + path)
+	fmt.Println("Headers : \n", headers)
 
 	var rep string
 
 	pathSplit := strings.Split(path, "/")
+	pathSplitLength := len(pathSplit)
 
 	if path == "/" {
 		rep = "HTTP/1.1 200 OK\r\n\r\n"
-	} else if len(pathSplit) >= 2 && pathSplit[1] == "echo" {
-		var content string
-		if len(pathSplit) > 2 {
-			content = pathSplit[2]
+	} else if pathSplitLength >= 2 {
+		switch pathSplit[1] {
+		case "echo":
+			rep = handleEcho(pathSplit)
+		case "user-agent":
+			rep = handleUserAgent(headers)
 		}
-		rep = fmt.Sprintf(
-			"HTTP/1.1 200 OK\r\n"+
-				"Content-Type: text/plain\r\n"+
-				"Content-Length: %d\r\n"+
-				"\r\n"+
-				"%s", len(content), content)
 	} else {
 		rep = "HTTP/1.1 404 Not Found\r\n\r\n"
 	}
@@ -65,4 +79,29 @@ func main() {
 	fmt.Println(rep)
 
 	conn.Write([]byte(rep))
+}
+
+func handleEcho(pathSplit []string) string {
+	content := pathSplit[2]
+	rep := fmt.Sprintf(
+		"HTTP/1.1 200 OK\r\n"+
+			"Content-Type: text/plain\r\n"+
+			"Content-Length: %d\r\n"+
+			"\r\n"+
+			"%s", len(content), content)
+
+	return rep
+}
+
+func handleUserAgent(headers map[string]string) string {
+	if headers["User-Agent"] == "" {
+		fmt.Println("No user-agent headers")
+	}
+
+	return fmt.Sprintf(
+		"HTTP/1.1 200 OK\r\n"+
+			"Content-Type: text/plain\r\n"+
+			"Content-Length: %d\r\n"+
+			"\r\n"+
+			"%s", len(headers["User-Agent"]), headers["User-Agent"])
 }
