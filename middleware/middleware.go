61c1a06282a0e0ac00a36a511a1db56a4ce44437
Hilde <hilderic.bourin@gmail.com>
2025-07-31 10:32:29 +0200

    GzipMiddleware clean up

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -14,27 +14,29 @@ type Middleware func(req *http.Request, resp *http.Response)
 
 func GzipMiddleware() Middleware {
 	return func(req *http.Request, resp *http.Response) {
-		body := resp.Body
 		encodings := req.Headers["Accept-Encoding"]
 
 		if strings.Contains(encodings, "gzip") {
 			req.Headers["Content-Encoding"] = "gzip"
+
 			var buf bytes.Buffer
 			zw := gzip.NewWriter(&buf)
-			_, err := zw.Write([]byte(body))
+
+			_, err := zw.Write([]byte(resp.Body))
 			if err != nil {
-				fmt.Println("Error encoding the body")
+				fmt.Println("Error encoding the body", err)
+				return
 			}
+
 			err = zw.Close()
 			if err != nil {
 				fmt.Println("Error closing the gzip writer", err)
 				return
 			}
 
-			body = buf.String()
-
-			resp.Headers["Content-Length"] = strconv.Itoa(len(body))
-			resp.Body = body
+			resp.Body = buf.String()
+			resp.Headers["Content-Encoding"] = "gzip"
+			resp.Headers["Content-Length"] = strconv.Itoa(len(resp.Body))
 		}
 	}
 }
